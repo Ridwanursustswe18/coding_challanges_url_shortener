635f2312d59f663aa011d8ca162a439ba1360d1e
Ridwan <ridwanur196@gmail.com>
2024-10-05 18:46:32 +0600

    added healthcheck

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,9 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 const urlShortenerRoute = require('./routes/urlShortenerRoute');
+app.use('/', (req, res) => {
+  res.status(200).json("server running");
+});
 app.use('/', urlShortenerRoute);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
