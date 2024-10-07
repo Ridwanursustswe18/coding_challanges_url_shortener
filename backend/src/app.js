61dff7077c298bd5b308177bded0e4e27c685686
Ridwan <ridwanur196@gmail.com>
2024-10-07 19:21:20 +0600

    changed it to enviorment variables

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const dotenv = require('dotenv');
+dotenv.config(); 
+const port = process.env.PORT || 3000;
 const cors = require('cors');
 app.use(cors());
 app.use(express.json());
--- backend/src/config.js
+++ backend/src/config.js
@@ -1,10 +1,16 @@
 const admin = require('firebase-admin');
-
-const serviceAccount = require('./urlshortener-80495-firebase-adminsdk-ako6j-619b4070f7.json');
-
+const dotenv = require('dotenv');
+dotenv.config(); 
+// Initialize Firebase using environment variables
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Replace escaped newlines
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
 });
+
 const db = admin.firestore();
 const url = db.collection('urls');
+
 module.exports = url;
